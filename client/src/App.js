cb84fc982cc32a828c3ce95c27d758f708f92e0a
rishitaa04 <rishita2010.be21@chitkara.edu.in>
2024-01-10 01:41:23 +0530

    adding frontend

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+// Import the Header component with a capitalized name
+import Header from './Components/Header/header';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <Header />
+    </div>
+  );
+}
+
+export default App;
