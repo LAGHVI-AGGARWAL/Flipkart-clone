22d7986bf8d98d494898c1e747d196b33ff84bdf
rishitaa04 <rishita2010.be21@chitkara.edu.in>
2024-01-10 04:18:36 +0530

    adding firstnavbar

--- client/src/Components/Header/header.jsx
+++ client/src/Components/Header/header.jsx
@@ -1,9 +1,37 @@
+import { AppBar, Toolbar, Box,Typography , styled } from "@mui/material";
 
+const StyledHeader = styled(AppBar)`
+  background: #2874f0;
+  height: 65px;
+`;
 
+const Component = styled(Box)`
+  margin-left: 12%;
+  line-height : 1;
+`;
 
-const header = () =>{
-    return(
-        <div>Hello from header</div>
-    )
-}
+const SubHeading = styled(Box)`
+  margin-left: -5%;
+  font-size : 12px;
+  font-style : italic;
+`;
+
+const header = () => {
+  const logoURL =
+    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
+  return (
+    <StyledHeader>
+      <Toolbar>
+        <Component>
+          <img src={logoURL} alt="logo" style={{ width: 75 }} />
+          <Box>
+            <SubHeading>Explore 
+            <Typography component="span">Plus</Typography>
+            </SubHeading>
+          </Box>
+        </Component>
+      </Toolbar>
+    </StyledHeader>
+  );
+};
 export default header;
