5401e17b80cd98419f1b29838c065ebdf451f540
LAGHVI-AGGARWAL <145498319+LAGHVI-AGGARWAL@users.noreply.github.com>
2024-01-11 11:31:17 +0530

    Update header.jsx

--- client/src/Components/Header/header.jsx
+++ client/src/Components/Header/header.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar, Box,Typography , styled, colors } from "@mui/material";
+import { AppBar, Toolbar, Box , Typography , styled, colors } from "@mui/material";
 
 //importing search
 import CustomButton from "./customButton";
@@ -7,23 +7,22 @@ import Search from './Search';
 
 const StyledHeader = styled(AppBar)`
   background: #2874f0;
-  height: 65px;
+  height: 55px;
 `;
 
 const Component = styled(Box)`
-  margin-left: 9%;
-  line-height : 1;
+  margin-left: 12%;
+  line-height: 0;
 `;
 
-const SubHeading = styled(Box)`
-  ${'' /* margin-left: 2%; */}
-  font-size : 11px;
+const SubHeading = styled(Typography)`
+  font-size : 10px;
   font-style : italic;
 `;
 
 const PlusImage = styled('img')({
-    width: 15,
-    height: 15,
+    width: 10,
+    height: 10,
     marginLeft: 4
 })
 
@@ -32,21 +31,20 @@ const CustomButtonWrapper = styled(Box)`
 `
 
 const header = () => {
-  const logoURL =
-    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
-    const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
+  const logoURL = "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
+    const subURL = "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png";
   return (
     <StyledHeader>
       <Toolbar style={{minHeight:65}}>
         <Component>
           <img src={logoURL} alt="logo" style={{ width: 75 }} />
-          <Box style = {{display : 'flex'}}>
+          <Box style = {{ display: 'flex' }}>
             <SubHeading>Explore &nbsp;
-            <Box component="span" style={{color : '#FFE500'}}>Plus</Box>
+              <Box component="span" style={{color : '#FFE500'}}>Plus</Box>
             </SubHeading>
             <PlusImage src={subURL} />
-                    </Box>
-                </Component>
+          </Box>
+        </Component>
         < Search />
         <CustomButtonWrapper>
             <CustomButton />
