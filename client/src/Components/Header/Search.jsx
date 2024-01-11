94505a1d2c124fddaee358848bbd6c85393839fd
LAGHVI-AGGARWAL <145498319+LAGHVI-AGGARWAL@users.noreply.github.com>
2024-01-11 13:09:36 +0530

    Update Search.jsx

--- client/src/Components/Header/Search.jsx
+++ client/src/Components/Header/Search.jsx
@@ -1,19 +1,20 @@
-import { InputBase , Box , styled} from "@mui/material";
+import { InputBase, Box, styled } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 
 const SearchContainer = styled(Box)`
-        background : #fff;
-        width : 38%;
-        border-radius :2px;
-        margin-left : 25px;
-        display : flex;
+        background: #fff;
+        width: 38%;
+        border-radius: 2px;
+        margin-left: 10px;
+        display: flex;
 `;
 
 const InputSearchBase = styled(InputBase)`
-    padding-left : 20px;
-    width : 100%;
-    font-size : unset;
-`
+    padding-left: 20px;
+    width: 100%;
+    font-size: unset;
+`;
+
 const SearchIconWrapper = styled(Box)`
   margin-left: auto;
   padding: 5px;
@@ -21,17 +22,12 @@ const SearchIconWrapper = styled(Box)`
   color: blue;
 `;
 
-const Search =  () => {
+const Search = () => {
     return (
         <SearchContainer>
-        <InputSearchBase
-
-            placeholder="Search for products,brands and more"
-
-
-        />
-        <SearchIconWrapper>
-              <SearchIcon />
+            <InputSearchBase placeholder="Search for products,brands and more" />
+            <SearchIconWrapper>
+                <SearchIcon />
             </SearchIconWrapper>
         </SearchContainer>
     )
