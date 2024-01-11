1b4e9fd64d222a0f67af6fbea6b688d5605237d8
LAGHVI-AGGARWAL <145498319+LAGHVI-AGGARWAL@users.noreply.github.com>
2024-01-11 13:10:01 +0530

    Update customButton.jsx

--- client/src/Components/Header/customButton.jsx
+++ client/src/Components/Header/customButton.jsx
@@ -1,51 +1,49 @@
-
-import {Box , Button, Typography} from '@mui/material';
-import {ShoppingCart} from '@mui/icons-material';
+import { Box, Button, Typography } from '@mui/material';
+import { ShoppingCart } from '@mui/icons-material';
 import styled from '@emotion/styled';
 
 const Wrapper = styled(Box)`
-    display : flex;
+    display: flex;
     margin : 0 3% 0 auto; 
-     & button , & > p , & > div {
-        margin-top : 10px;
-        margin-right : 76px;
-        font-size : 16px;
-        align-items : center;
-     }
-
-`
+    & > button , & > p , & > div {
+        margin-top: 10px;
+        margin-right: 40px;
+        font-size: 16px;
+        align-items: center;
+    }
+`;
 
 const container = styled(Box)`
-    display : flex;
-`
+    display: flex;
+`;
 
 const LoginButton = styled(Button)`
-    color : #2874f0;
-    background : #fff;
-    text-transform : none;
-    padding : 5px 40px;
-    border-radius : 2px;
-    box-shadow : none;
-    font-weight : 600;
-    height : 32px;    
-`
+    color: #2874f0;
+    background: #fff;
+    text-transform: none;
+    padding: 5px 40px;
+    border-radius: 2px;
+    box-shadow: none;
+    font-weight: 600;
+    height: 32px;    
+`;
 
 
 
-const CustomButton = () =>{
-    return(
+const CustomButton = () => {
+    return (
         <Wrapper>
-            <LoginButton variant='contained' >Login</LoginButton>
+            <LoginButton variant = 'contained' > Login </LoginButton>
 
-            <Typography style={{ marginTop: 10, width: 135 }}>Become a Seller</Typography>
-            <Typography style={{ marginTop: 10 }}>More</Typography>
+            <Typography style={{ marginTop: 15, width: 135 }}> Become a Seller </Typography>
+            <Typography style={{ marginTop: 15 }}> More </Typography>
 
 
             <container>
-            <ShoppingCart />
-            <Typography style={{ marginLeft: 10 }}>Cart</Typography>
+                <ShoppingCart/>
+                <Typography> Cart </Typography>
             </container>
-            </Wrapper>
+        </Wrapper>
     )
 }
 export default CustomButton;
